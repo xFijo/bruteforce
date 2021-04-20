a171723bbec85c1941cf449b5cc8dca6dad98aab
xFijo <75216577+xFijo@users.noreply.github.com>
2021-04-20 22:25:37 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import random 
+import pyautogui
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+chars_list = list(chars)
+
+password = pyautogui.password("enter a password : ")
+
+guess_password = ""
+
+while(guess_password != password):
+    guess_password = random.choices(chars_list, k=len(password))
+
+    print(f'<========================= {str(guess_password)} ====================>')
+
+    if (guess_password == list(password)):
+        print("your password is : "+ "".join(guess_password))
+        break
